7c358900ff5d5b0636bf3b7ddcc234279ba2bdbf
FaisalFehad <faisal.fehad@pm.me>
2020-01-25 18:02:31 +0000

    add boostrap card for article card component

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -7,21 +7,25 @@ const ArticleCard = ({ allArticles }) => {
     return allArticles.map(article => {
       return (
         <main key={article.article_id}>
-          <hr />
-          <h3>
-            <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
-          </h3>
-          <h6>-By {article.author}</h6>
-          <h5>
-            <Link to={`/topics/${article.topic}`}>{article.topic}</Link>
-          </h5>
-          <h5>Comments {article.comment_count}</h5>
-          <Vote
-            id={article.article_id}
-            currentVote={article.votes}
-            path={"articles"}
-          />
-          <hr />
+          <div class="card">
+            <div class="card-body">
+              <h3>
+                <Link to={`/articles/${article.article_id}`}>
+                  {article.title}
+                </Link>
+              </h3>
+              <h6>-By {article.author}</h6>
+              <h5>
+                <Link to={`/topics/${article.topic}`}>{article.topic}</Link>
+              </h5>
+              <h5>Comments {article.comment_count}</h5>
+              <Vote
+                id={article.article_id}
+                currentVote={article.votes}
+                path={"articles"}
+              />
+            </div>
+          </div>
         </main>
       );
     });
